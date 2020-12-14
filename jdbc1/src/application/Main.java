427dcd04a52a1fa9cc02ba500682e25b7847d555
Artur Sousa <artursousa505@gmail.com>
2020-12-14 20:15:21 -0300

    Methods added: Delete, Update(name, country, email), Withdraw, Deposit

--- jdbc1/src/application/Main.java
+++ jdbc1/src/application/Main.java
@@ -2,22 +2,31 @@ package application;
 
 
 import db.DB;
+import entities.Worker;
+
 
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		
 
 		DB db = new DB();
 		
-		System.out.println(db.getWorkerFromDB(Long.parseLong("5442255045321780")));
 		
-		db.updateWorkerName("Artur Ribeiro", Long.parseLong("5442255045321780"));
+		db.sendWorkerToDB(new Worker("John Paul Jones", 9.00, "bass@ledZeppelin.com", Long.parseLong("5140806899961234"), "United Kingdom", "2010-04-17T23:22:40Z"));
+		db.getWorkerFromDB(Long.parseLong("5140806899961234"));
+		System.out.println(db.getWorkerFromDB(Long.parseLong("5140806899961234")));
+		System.out.println(db.getWorkerFromDB(Long.parseLong("5140806899966830")));
+		db.updateWorkerName("Name", Long.parseLong("5140806899966830"));	
+		db.updateWorkerEmail("testemail@test.com", Long.parseLong("5140806899966830"));
+		db.updateWorkerCountry("United Kingdom",  Long.parseLong("5140806899966830"));
+		db.withdraw(0.12, Long.parseLong("5140806899966830"));
+		db.deposit(1.,  Long.parseLong("5140806899966830"));
 		
-		System.out.println(db.getWorkerFromDB(Long.parseLong("5442255045321780")));
 		
+		System.out.println(db.getWorkerFromDB(Long.parseLong("5140806899966830")));
+				
 		DB.closeConnection();
 	}
 }
--- jdbc1/src/db/DB.java
+++ jdbc1/src/db/DB.java
@@ -104,7 +104,7 @@ public class DB {
 			ResultSet rs = st.executeQuery("SELECT * FROM users");
 			int i = 0;
 			while (rs.next() && i == 0) {
-				if(rs.getLong("CCV") == CCV) {
+				if(rs.getLong("CCV") == CCV ) {
 					wk.setName(rs.getString(2));
 					wk.setAccountBalance(rs.getDouble(3));
 					wk.setEmail(rs.getString(4));
@@ -112,7 +112,7 @@ public class DB {
 					wk.setCountry(rs.getString(6));
 					wk.setCreatedIn(rs.getString(7));
 					i = 1;
-				}
+				} 
 			}			
 		}catch (SQLException e) {
 			System.out.println(e.getMessage());
@@ -139,6 +139,139 @@ public class DB {
 			
 			if (rowsAffected > 0) {
 				System.out.println("Done, rows affected: " + rowsAffected);
+			}
+			
+		}catch (SQLException e) {
+			e.printStackTrace(); 
+		} 
+	}
+	
+	public void updateWorkerEmail(String newEmail, Long ccvToUpdate) {
+		Connection conn =  null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement(
+					"UPDATE users "
+					+ "SET Email = ? "
+					+ "WHERE "
+					+ "(CCV=?)");
+			
+			st.setString(1, newEmail);
+			st.setLong(2, ccvToUpdate);
+
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				System.out.println("Done, rows affected: " + rowsAffected);
+			}
+			
+		}catch (SQLException e) {
+			e.printStackTrace(); 
+		} 
+	}
+	public void updateWorkerCountry(String newCountry, Long ccvToUpdate) {
+		Connection conn =  null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement(
+					"UPDATE users "
+					+ "SET Country = ? "
+					+ "WHERE "
+					+ "(CCV=?)");
+			
+			st.setString(1, newCountry);
+			st.setLong(2, ccvToUpdate);
+
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				System.out.println("Done, rows affected: " + rowsAffected);
+			}
+			
+		}catch (SQLException e) {
+			e.printStackTrace(); 
+		} 
+	}
+	
+	
+	public void withdraw(double withdraw, Long ccvToUpdate) {
+		Connection conn =  null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"UPDATE users "
+					+ "SET AccountBalance = AccountBalance - ? "
+					+ "WHERE "
+					+ "(CCV=?)");
+			
+			st.setDouble(1, withdraw);
+			st.setLong(2, ccvToUpdate);
+			
+			
+			if (withdraw <= getWorkerFromDB(ccvToUpdate).getAccountBalance()) {
+				int rowsAffected = st.executeUpdate();
+				if (rowsAffected > 0) {
+					System.out.println("Withdraw Sucessfully! New Balance: $ " + String.format("%.2f", getWorkerFromDB(ccvToUpdate).getAccountBalance()));
+				}
+			} else {
+				System.out.println("Insufficient funds for this Transaction");
+			}
+			
+			
+		}catch (SQLException e) {
+			e.printStackTrace(); 
+		} 
+	}
+	
+	public void deposit(double deposit, Long ccvToUpdate) {
+		Connection conn =  null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"UPDATE users "
+					+ "SET AccountBalance = AccountBalance + ? "
+					+ "WHERE "
+					+ "(CCV=?)");
+			
+			st.setDouble(1, deposit);
+			st.setLong(2, ccvToUpdate);
+			
+
+			int rowsAffected = st.executeUpdate();
+			if (rowsAffected > 0) {
+				System.out.println("Deposit Sucessfully! New Balance: $ " + String.format("%.2f", getWorkerFromDB(ccvToUpdate).getAccountBalance()));
+			}
+			
+		}catch (SQLException e) {
+			e.printStackTrace(); 
+		} 
+	}
+	
+	public void deleteWorkerFromDB(Long ccvToDelete) {
+		Connection conn =  null;
+		PreparedStatement st = null;
+		Worker wk = getWorkerFromDB(ccvToDelete);
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement(
+					"DELETE FROM users "
+					+ "WHERE "
+					+ "(CCV=?)");
+			
+			st.setLong(1, ccvToDelete);
+
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				System.out.println("Done, rows affected: " + rowsAffected +
+						"\n User: " + wk.getName() + " removed Sucessfully from DB");
 			}
 			
 		}catch (SQLException e) {
