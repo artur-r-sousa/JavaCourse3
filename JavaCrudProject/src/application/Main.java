a17c6c2c8233e04c991dfcd446df323e14103667
Artur Sousa <artursousa505@gmail.com>
2020-12-15 11:40:16 -0300

    Removed unnecessary commentaries from main

--- JavaCrudProject/src/application/Main.java
+++ JavaCrudProject/src/application/Main.java
@@ -72,17 +72,6 @@ public class Main {
 			}
 		
 		}
-		/*
-		db.sendWorkerToDB(new Worker("John Paul Jones", 9.00, "bass@ledZeppelin.com", Long.parseLong("5140806899961234"), "United Kingdom", "2010-04-17T23:22:40Z"));
-		db.getWorkerFromDB(5140806899961234L);
-		System.out.println(db.getWorkerFromDB(5140806899961234L));
-		System.out.println(db.getWorkerFromDB(5140806899966830L));
-		db.updateWorkerName("Name", 5140806899966830L);	
-		db.updateWorkerEmail("testemail@test.com", 5140806899966830L);
-		db.updateWorkerCountry("United Kingdom",  5140806899966830L);
-		db.withdraw(0.12, 5140806899966830L);
-		db.deposit(1., 5140806899966830L);
-		*/
 
 		sc.close();		
 		DB.closeConnection();
